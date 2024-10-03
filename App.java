3f34198928e8593c8df9038be160513bd1876359
HilmaKorkeila <hilma.korkeila@student.hamk.fi>
2024-10-03 21:29:32 +0300

    Create App.java

---
+++ App.java
@@ -0,0 +1,38 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hei olen Tulostin ohjelma");
+        System.out.println("Ohjelman tekijä");
+        String name = "Hilma";
+        System.out.println(name);
+        System.out.print("Ohjelman tekijä ");
+        System.out.println(name);
+        int luku1, luku2, result;
+        luku1 = 5;
+        luku2 = 2; 
+        result = luku1 * luku2;
+        System.out.println(result);
+        System.out.print("Luku1 muuttujan arvo on ");
+        System.out.println(luku1);
+        System.out.print("Luku2 muuttujan arvo on ");
+        System.out.println(luku2);
+
+        int tulo;
+
+        tulo = luku1 + luku2; 
+
+        System.out.println(luku1 + " + " + luku2 + " = " + tulo);
+
+
+        
+
+
+       
+     
+
+
+       
+        
+
+      
+    }
+}
